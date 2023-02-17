d6a86a1619b2035307430cda5b57df17e4012314
Neoxy <1007050740@qq.com>
2023-02-17 18:47:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BetterWeapons
+minecraft server plugin
---
+++ src/main/java/me/neoxy/betterweapons/BetterWeapons.java
@@ -0,0 +1,17 @@
+package me.neoxy.betterweapons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterWeapons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
